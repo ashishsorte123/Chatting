bd2d98a5298e3b50d9a3dac25cfab667e49ca854
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2022-02-03 12:56:48 +0530

    init amplify & auth

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import { Text, View, StyleSheet, FlatList, Pressable } from "react-native";
+import { Auth } from "aws-amplify";
 import ChatRoomItem from "../components/ChatRoomItem";
 import chatRoomsData from "../assets/dummy-data/ChatRooms";
 
 export default function TabOneScreen() {
+  const logOut = () => {
+    Auth.signOut();
+  };
   return (
     <View style={styles.page}>
       <FlatList
@@ -11,6 +15,20 @@ export default function TabOneScreen() {
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
         showsVerticalScrollIndicator={false}
       />
+
+      <Pressable
+        onPress={logOut}
+        style={{
+          backgroundColor: "red",
+          height: 50,
+          margin: 10,
+          borderRadius: 50,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>Logout</Text>
+      </Pressable>
     </View>
   );
 }
